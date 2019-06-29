30ad98fe9a3a24503894eb364ab9d14b19a9d2cc
SOAP <471804582@qq.com>
2019-06-29 21:47:17 +0800

    question

--- Earn-Money-System/miniprogram/pages/QuestionDetail/QuestionDetail.js
+++ Earn-Money-System/miniprogram/pages/QuestionDetail/QuestionDetail.js
@@ -5,6 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
+    topbartitleinfo: "任务详情",
+    isUserPublisher: true,
+    isUserAcceptter: -1,
+    disabled: true,
+    missionName: "Temp",
+    time: "temp",
+    pay: 0,
+    progress: "temp",
+    detail: "temp",
+    mission: null,
+    starttime: "2019-06-26",
 
   },
 
@@ -12,7 +23,47 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //get isUserPublisher from Server
+    var db = wx.cloud.database()
+    var app = getApp()
+    var that = this
 
+    this.setData({
+      starttime: util.formatTime(new Date())
+    })
+
+    db.collection('Mission').where({
+      _id: app.globalData.mission_id
+    }).get().then(res => {
+      console.log(res)
+      var getMission = res.data[0]
+
+      wx.cloud.callFunction({
+        name: "login",
+        success: function (res) {
+          var openid = res.result.openid
+
+          var check_pub = false
+          var check_re = false
+          if (getMission.publisher_id == openid)
+            check_pub = true
+
+          check_re = getMission.Participant.indexOf(openid)
+
+          that.setData({
+            isUserPublisher: check_pub,
+            isUserAcceptter: check_re,
+            missionName: getMission.Title,
+            time: getMission.Time,
+            pay: getMission.Pay,
+            progress: getMission.state,
+            detail: getMission.Info,
+            mission: getMission
+          })
+        }
+      })
+
+    })
   },
 
   /**
